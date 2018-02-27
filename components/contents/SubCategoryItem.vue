a122085375bc81c6f51f9cd5e78ae5a31e76fa05
tareq89 <tareqaziz.aust.cse@gmail.com>
2018-02-27 13:00:20 +0600

    generating items payload randomly on front end

--- components/contents/SubCategoryItem.vue
+++ components/contents/SubCategoryItem.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="col-lg-2 col-md-3 col-sm-6 col-xs-12 item">
-        <img class="img-thumbnail" :src="randomImageSrc" alt="">
+        <img class="img-thumbnail" :src="item.imageUrl" :alt="item.name">
         <div class="details">
-            <h5 class="text-center">{{ name }}</h5>
-            <button class="add-to-cart">Add to Cart</button>
+            <h5 class="text-center">{{ item.name }}</h5>
+            <h4 class="text-center">৳ {{ item.price }}</h4>
+            <div class="add-to-cart-container">
+                <span class="add-to-cart">Add to Cart</span>
+            </div>
         </div>
     </div>
 </template>
@@ -19,6 +22,19 @@ export default {
             let randomImageId = `${firstNumber}-${secondNumber}`;
             let imageUrl = `https://placeimg.com/400/300/any?${randomImageId}`;
             return imageUrl;
+        },
+        randomPrice() {
+            let randomPrice = (Math.random() * 1000).toPrecision(3);
+            randomPrice = randomPrice.includes('.')? randomPrice.slice(0, randomPrice.indexOf('.')) : randomPrice;
+            return parseInt(randomPrice);
+        },
+        item() {
+            let item = {
+                name: this._props.name,
+                price: this.randomPrice,
+                imageUrl: this.randomImageSrc
+            }            
+            return item;
         }
     }
 }
@@ -36,14 +52,18 @@ export default {
     position: relative;
 }
 .add-to-cart {
-    width: 100%;
-    background-color: white;
-    /* border: none;     */
-    border-radius: 0px 0.25rem 0.25rem 0px;
-    position: absolute;
+    width: 100%;    
+    height: 40px;
+    line-height: 40px;
+    border-radius: 0px 0px 5px 5px;;
+    position: absolute;    
+    text-align: center;
+    cursor: pointer;    
+    background-color: #ffd875;
 }
-.add-to-cart :active {
-    background-color: grey;
-    outline: 0px !important;
+
+.add-to-cart-container :active {
+    font-weight: 700;
+    background-color: #f1b212;    
 }
 </style>
