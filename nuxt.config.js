ac9378388de0fd7cb9d1a0ed2648b54686d2e03b
tareq89 <tareqaziz.aust.cse@gmail.com>
2018-02-27 22:25:02 +0600

    added dotenv package

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const pkg = require('./package')
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   mode: 'universal',
   env: {
-    apiBaseUrl: 'http://127.0.0.1:9000/api/'
+    apiBaseUrl: process.env.API_BASE_URL
   },
   /*
   ** Headers of the page
